075aa8469c9d2e2a70e17f59b350918fa752c717
SebaMayoru <smayoru@alumno.unlam.edu.ar>
2021-06-13 16:31:59 -0300

    Agregamos activities, funciona el sensor de luz

--- codigo/app/src/main/java/com/example/proyecto/ActivityFuncional.java
+++ codigo/app/src/main/java/com/example/proyecto/ActivityFuncional.java
@@ -2,13 +2,29 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityFuncional extends AppCompatActivity {
-
+    Button botonTomarTemperatura;
+    Intent intentTomarTemperatura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_funcional);
+        botonTomarTemperatura = findViewById(R.id.buttonTomarTemp);
+
+        botonTomarTemperatura.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                lanzarActivityTomarTemperatura();
+            }
+        });
+    }
+    void lanzarActivityTomarTemperatura(){
+        intentTomarTemperatura = new Intent (this,TomarTemperatura.class);
+        this.startActivity(intentTomarTemperatura);
     }
 }
