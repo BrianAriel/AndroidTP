18d9ef7246874f7ceebd3f7139d0b771fc1c605b
SebaMayoru <smayoru@alumno.unlam.edu.ar>
2021-06-14 23:36:44 -0300

    Cambios en la toma de temperatura, handler agregado para que muestre la temperatura tomada en un TextView. Agregada función de vibración para que vibre cuando toma la temperatura

--- codigo/app/src/main/java/com/example/proyecto/TomarTemperatura.java
+++ codigo/app/src/main/java/com/example/proyecto/TomarTemperatura.java
@@ -2,27 +2,44 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class TomarTemperatura extends AppCompatActivity {
     SensorManager sm;
     SensorEventListener listener;
     Sensor light;
     Button botonMedir;
-    float temperatura;
+    TextView textTemperatura;
+    String temperatura;
     float medicion;
+    Vibrator vibrador;
+
+    Handler h = new Handler() {
+        public void handleMessage(Message msg){
+            //Toast.makeText(getApplicationContext(), "Temperatura: " + temperatura, Toast.LENGTH_LONG).show();
+            textTemperatura.setText(temperatura);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tomar_temperatura);
         botonMedir = findViewById(R.id.buttonMedir);
+        vibrador = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        textTemperatura = findViewById(R.id.textViewTemperatura);
         sm = (SensorManager) getSystemService(SENSOR_SERVICE);
         light = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
         listener = new SensorEventListener() {
@@ -37,13 +54,16 @@ public class TomarTemperatura extends AppCompatActivity {
                 medicion = event.values[0];
             }
         };
-
         sm.registerListener(listener, light, SensorManager.SENSOR_DELAY_FASTEST);
 
+
+
         botonMedir.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v) {
                 //aquí debe esperar 2 minutos y tomar la temperatura (luz)
+
                 Thread mySplash = new Thread() {
                     @Override
                     public void run() {
@@ -52,25 +72,30 @@ public class TomarTemperatura extends AppCompatActivity {
                             Log.i("Tomando temperatura...",String.valueOf(temperatura));
                             sleep(3000);
                             temperatura = convertirTemperatura(medicion);
-                            Log.i("Temp registrada: ",String.valueOf(temperatura));
+                            vibrador.vibrate(300);//REVISAR
+                            Log.i("Temp registrada: ",temperatura);
+                            h.sendEmptyMessage(0);//ejecuto el handler
 
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
                 };
-                mySplash.start();//pasan los 3 segundos
+
+                mySplash.start();//Ejecuto el thread para que tome la temperatura
 
 
             }
         });
 
     }
-    private float convertirTemperatura(float medicion){
+    private String convertirTemperatura(float medicion){
         float factorConversion = 40000/15;
-        float temperatura = 30 + (medicion/factorConversion);
-        return temperatura;
+        return String.format("%.02f",(30 + (medicion/factorConversion)));
     }
+
+
+
     @Override
     protected void onPause() {
         super.onPause();
