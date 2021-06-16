4340f8c0adf319c9ef9c56cdfa03902af4aaa986
Brian Piñeyro <brianariel99gmail.com>
2021-06-16 11:17:57 -0300

    Se creo activityHistorial con su layout para mostrar un listado en pantalla con todas las mediciones hasta el momento indicando fecha, hora y temperatura. Se implemento sharedPreferences para leer en ActivityHistorial y para escribir en TomarTemperatura. Ambos de estos metodos son synchronized. Restaria la logica del acelerometro

---
+++ codigo/app/src/main/java/com/example/proyecto/ActivityHistorial.java
@@ -0,0 +1,65 @@
+package com.example.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ActivityHistorial extends AppCompatActivity {
+
+    SharedPreferences sharedPref;
+    ListView lista;
+    Button botonRegresar;
+    ArrayList<String> listaMediciones;
+    ArrayAdapter<String> arrayAdapter;
+    Intent intentFuncional;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial);
+
+        intentFuncional = new Intent(this,ActivityFuncional.class);
+
+        sharedPref = getApplicationContext().getSharedPreferences(getString(R.string.archivo_preferences), Context.MODE_PRIVATE);
+        listaMediciones = new ArrayList<>();
+        llenarLista();
+
+        lista = findViewById(R.id.listaMediciones);
+        botonRegresar = findViewById(R.id.buttonRegresar);
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaMediciones);
+
+        botonRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentFuncional);
+            }
+        });
+
+        lista.setAdapter(arrayAdapter);
+    }
+
+    private synchronized void llenarLista(){
+        Map<String,?> keys = sharedPref.getAll();
+
+        for (Map.Entry<String,?> entry : keys.entrySet()){
+            listaMediciones.add(entry.getValue().toString());
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
